4b41fd20ab23f444332b2e131b15ea4fbe5c6b6f
Leosoler <leitosolerleon@gmail.com>
2021-12-07 20:37:59 -0500

    cytusII principal + inicios OS (conner)

---
+++ !experimentar/proyectoCytus/OSpersonajes/OSscript.js
@@ -0,0 +1,13 @@
+const versionCaja = document.querySelector('.version');
+const versionTitle = document.querySelector('.h3');
+
+/* mierdas que tiene que hacer esta porqueria */
+
+/* 1. abrir y cerrar las versiones que se le de click */
+versionTitle.addEventListener('click', e =>{
+
+    versionCaja.classList.add('version_open');
+
+})
+
+/* 2. */
